b508945e0a7e0c18fa77e08125d5ba47e75efc05
lec415 <lec415@lehigh.edu>
2015-08-31 21:40:17 +0000

    first homework assignment. Code for printing a welcome message.

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//CSE002
+//Leonardo C Ramos
+//31/08/15
+//First Homework Assignment @
+//Welcome Class Java program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass
+
+//define the class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main (String[] args){
+        
+        //Printing the message
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-L--E--C--4--1--5-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println(" This August I moved from Brazil to the US to study at Lehigh for a year.");
+        
+    }
+}
